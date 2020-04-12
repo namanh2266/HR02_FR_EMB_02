7334172a07f65506afafbbec06d8930f73959cc7
namanh2266 <namanh260496@gmail.com>
2020-04-13 06:44:39 +0700

    Update Branch 02

---
+++ Print2.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#define MAX 10
+
+void Init(int Array[], int n)
+{
+	int i;
+	for(i=0; i<n; i++)
+	{
+		Array[i]=i+4;
+	}
+}
+
+
+int Print(int Array[], int n)
+{
+	int i;
+	for(i=0; i<n; i++){
+		printf("%d  ",Array[i]);
+	}
+	printf("\ndang o nhanh 2");
+}
+
+int main()
+{
+	int Array[MAX]={4,9,6,8,2,5,7,1,0,3};
+	Print(Array,MAX);
+	
+}
