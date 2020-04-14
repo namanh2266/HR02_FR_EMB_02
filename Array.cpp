d35f469a00a3847df9f8023de0b49a1a782cb1be
namanh2266 <namanh260496@gmail.com>
2020-04-14 11:28:07 +0700

    fefeaaa

--- Array.cpp
+++ Array.cpp
@@ -48,7 +48,6 @@ int main()
 	printf("sap xep giam\n");
 	SapXepGiam(Array,MAX);
 	Print(Array,MAX);
-	printf("branch 1 1 1 );
-	printf("branch 1 1 1 ");
-	printf("branch 1 1 1 ";
+	printf("branch 11");
+	printf("branch 11");
 }
